37a38a5dc0385a3f66df5f67bd778fa799523303
Seth Addo <addoseth35@gmail.com>
2024-08-13 19:08:03 +0000

    feat: add speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as SonnerToaster } from "sonner";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {children}
+        <SpeedInsights />
         <Toaster />
         <SonnerToaster richColors expand={false} position="top-right" />
       </body>
