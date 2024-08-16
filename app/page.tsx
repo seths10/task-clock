c91171422a17e65401c9ed11cc3d9b602cc8c2a8
Seth Addo <addoseth35@gmail.com>
2024-08-16 16:37:41 +0000

    chore: add background grid on home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,11 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="container flex h-screen flex-col items-center justify-center">
+    <div className="flex absolute inset-0 -z-10 h-screen items-center justify-center w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
       <div className="lg:p-8">
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 text-center sm:w-[450px]">
           <div className="flex flex-col space-y-2 text-center">
-            <h1 className="text-4xl font-bold tracking-tight text-neutral-900 dark:text-white">
+            <h1 className="text-4xl font-bold tracking-tight dark:text-neutral-800 text-neutral-900">
               Welcome to Task Clock!
             </h1>
             <p className="text-muted-foreground text-sm">
